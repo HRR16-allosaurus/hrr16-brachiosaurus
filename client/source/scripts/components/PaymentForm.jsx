e33642e7fc50f35c66c8dd5f163876be2654136a
Chris Weaver <cmagweaver@gmail.com>
2016-06-11 17:31:10 -0600

    Basic Stripe Feature Works (#7)
    * Added Strip View
    
    * Stripe restored
    
    * Stripe works for valid numbers and invalid numbers
    Tested
    
    * Basic Stripe Feature Works

---
+++ client/source/scripts/components/PaymentForm.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import {asyncLoad} from 'react-async-loader';
+
+const PaymentForm = React.createClass({
+  mixins: [ asyncLoad ],
+
+  getInitialState() {
+    return {
+      stripeLoading: false,
+      stripeLoadingError: false,
+      submitDisabled: false,
+      paymentError: null,
+      paymentComplete: false,
+      token: null
+    };
+  },
+
+  onSubmit(event) {
+    const self = this;
+    event.preventDefault();
+    this.setState({ submitDisabled: true, paymentError: null});
+
+      Stripe.createToken(event.target, (status, response) => {
+        if (response.error) {
+          self.setState({ paymentError: response.error.message, submitDisabled: false });
+        }
+        else {
+          self.setState({stripeLoading: true, token: response.id});
+        
+          $.ajax({
+              url: '/stripe',
+              contentType: 'application/json',
+              type: 'POST',
+              data: JSON.stringify({
+                token: this.state.token,
+              }),
+              success: function(result){
+                var result = JSON.parse(result);
+                if(result.error){
+                  self.setState({ paymentError:"Payment Error", stripeLoading: false});
+                } 
+                if(result.success){
+                self.setState({ paymentComplete: true, stripeLoading: false});
+                }
+              },
+              error: function(result){
+                console('Cannot communicate to server');
+              }
+          });
+        
+      }
+    });
+  },
+
+  render() {
+    if (this.state.stripeLoading) {
+      return <div>Processing...</div>;
+    }
+    
+    if (this.state.paymentComplete) {
+      return <div>Payment Complete!</div>;
+    }
+    
+      return (<form onSubmit={this.onSubmit} >
+        <span>{ this.state.paymentError }</span><br />
+        <input type='text' data-stripe='number' placeholder='credit card number' /><br />
+        <input type='text' data-stripe='exp-month' placeholder='expiration month' /><br />
+        <input type='text' data-stripe='exp-year' placeholder='expiration year' /><br />
+        <input type='text' data-stripe='cvc' placeholder='cvc' /><br />
+        <input disabled={this.state.submitDisabled} type='submit' value='Purchase' />
+      </form>);
+  }
+});
+
+export default PaymentForm;
--- server/server.js
+++ server/server.js
@@ -4,6 +4,11 @@ const app = express();
 const port = process.env.PORT || 8080;
 const db = require('./db/');
 const path = require('path');
+//stripe authenication
+//change to production key later!!!
+const stripe = require('stripe')('sk_test_wSTxvrFX0Wo8oyxlfea05lPu');
+const StripeCharge = require('./stripe/StripeCharge');
+
 app.use(express.static(`${__dirname}/../client/build`));
 app.use(parser.json());
 
@@ -15,5 +20,34 @@ app.get('/api/workouts/hard', (req, res) => db.helpers.getVal(db.ref.workoutHard
 app.get('/api/workouts/weightloss', (req, res) => db.helpers.getVal(db.ref.weightloss, res));
 app.get('*', (req, res) => res.sendFile(path.join(`${__dirname}/../client/build/index.html`)));
 
+
+
+
+// Stripe routing and charge
+app.post('/stripe', (req, res) => {
+    console.log(req.body.token)
+    StripeCharge(req.body.token,function(err, charge){
+      if(err) {
+        console.log('Payment Declined');
+         var response = {
+          status  : 400,
+          error : 'Payment Declined'
+        }
+        res.end(JSON.stringify(response));
+
+    }
+      else {
+        console.log('Payment Recieved');
+        var response = {
+          status  : 200,
+          success : 'Payment Recieved'
+        }
+        res.end(JSON.stringify(response));
+        
+      }
+    },stripe);
+});
+
 app.listen(port, () => console.log('Server running on port ' + port));
+
 module.exports = app;
