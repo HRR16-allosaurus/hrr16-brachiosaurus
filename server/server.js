0567ec163078410d960098f687ba6e1d0a8beabe
Timothy Woo <timwoo24@gmail.com>
2016-06-11 16:30:23 -0700

    build out mongo schema and model and requets handler for user to create set or workouts

--- server/server.js
+++ server/server.js
@@ -4,10 +4,14 @@ const app = express();
 const port = process.env.PORT || 8080;
 const db = require('./db/');
 const path = require('path');
+const controller = require('./db2/workoutController.js');
 app.use(express.static(`${__dirname}/../client/build`));
 app.use(parser.json());
 
 // API
+app.post('/new_workout', controller.addWorkout);
+app.get('/new_workout', controller.getWorkouts);
+
 app.get('/api/workouts', (req, res) => db.helpers.getVal(db.ref.workouts, res));
 app.get('/api/workouts/easy', (req, res) => db.helpers.getVal(db.ref.workoutEasy, res));
 app.get('/api/workouts/normal', (req, res) => db.helpers.getVal(db.ref.workoutNormal, res));
